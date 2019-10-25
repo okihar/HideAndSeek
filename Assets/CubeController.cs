78b5c29412ddfbb9c958972a9a45f6b268173f2d
okihar <okiharu521@gmail.com>
2019-10-26 00:39:49 +0900

    キャラ追加

---
+++ Assets/CubeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        transform. Translate(move*Time.deltaTime*speed);
+    }
+}
